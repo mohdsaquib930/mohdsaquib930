a0eb3ea23f787dbd3c4c68ab2e1b5876d1e84c98
Mohammad Saquib <140141697+mohdsaquib930@users.noreply.github.com>
2025-01-02 16:08:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const fileUrl = './app-release.apk'; // Replace with the actual URL of your file
+  const fileUrl = 'app-release.apk'; // Replace with the actual URL of your file
   const fileName = 'notebooks.apk; // Optional: Set a custom filename
 
   const link = document.createElement('a');
